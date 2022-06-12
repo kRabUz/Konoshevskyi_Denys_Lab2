94a9e9b57807bf09513ef207e22b9e8c5f6af7c8
Konoshevskyi Denys <107351163+kRabUz@users.noreply.github.com>
2022-06-12 16:33:09 +0300

    Add files via upload

---
+++ Lab2_1/Star.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_2_1
+{
+    class Star
+    {
+        private string star;
+        public string StarOfSystem { get { return star; } set { star = value; } }
+        public Star(string star)
+        {
+            this.star = star;
+        }
+        public string GetStar()
+        {
+            return this.star;
+        }
+    }
+}
